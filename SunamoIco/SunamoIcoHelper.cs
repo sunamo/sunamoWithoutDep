d108fdce9eb9ddcae990ab1330e660faefc524a3
sunamo <radek.jancik@sunamo.cz>
2020-06-02 01:50:39 +0200

    Mass commit 2-6-2020

---
+++ SunamoIco/SunamoIcoHelper.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+
+    public class SunamoIcoHelper
+    {
+
+    /// <summary>
+    /// Low quality
+    /// </summary>
+    /// <param name="newDir"></param>
+    /// <param name="path"></param>
+    public static Icon ConvertImageToIco(Image bitmap)
+    {
+        Icon icon = null;
+        
+            //icon = Icon.FromHandle(bitmap.GetHicon());
+        return icon;
+    }
+
+    
+
+    /// <summary>
+    /// Not working
+    /// Value cannot be null. (Parameter 'encoder')'
+    /// </summary>
+    /// <param name="imgFavorites"></param>
+    /// <returns></returns>
+    public static Icon ConvertToIcon(Image imgFavorites)
+        {
+            MemoryStream ms = new MemoryStream();
+
+            //Bitmap bmp = new Bitmap(imgFavorites);
+            imgFavorites.Save(ms, ImageFormat.Icon);
+            return new Icon(ms);
+        }
+
+    /// <summary>
+    /// High quality
+    /// </summary>
+    /// <param name="img"></param>
+    /// <returns></returns>
+        public static Icon IconFromImage(Image img)
+        {
+            var ms = new System.IO.MemoryStream();
+            var bw = new System.IO.BinaryWriter(ms);
+            // Header
+            bw.Write((short)0);   // 0 : reserved
+            bw.Write((short)1);   // 2 : 1=ico, 2=cur
+            bw.Write((short)1);   // 4 : number of images
+                                  // Image directory
+            var w = img.Width;
+            if (w >= 256) w = 0;
+            bw.Write((byte)w);    // 0 : width of image
+            var h = img.Height;
+            if (h >= 256) h = 0;
+            bw.Write((byte)h);    // 1 : height of image
+            bw.Write((byte)0);    // 2 : number of colors in palette
+            bw.Write((byte)0);    // 3 : reserved
+            bw.Write((short)0);   // 4 : number of color planes
+            bw.Write((short)0);   // 6 : bits per pixel
+            var sizeHere = ms.Position;
+            bw.Write((int)0);     // 8 : image size
+            var start = (int)ms.Position + 4;
+            bw.Write(start);      // 12: offset of image data
+                                  // Image data
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            var imageSize = (int)ms.Position - start;
+            ms.Seek(sizeHere, System.IO.SeekOrigin.Begin);
+            bw.Write(imageSize);
+            ms.Seek(0, System.IO.SeekOrigin.Begin);
+
+            // And load it
+            return new Icon(ms);
+        }
+
+    /// <summary>
+    /// High quality
+    /// </summary>
+    /// <param name="img"></param>
+    /// <returns></returns>
+    public static Icon ConvertToIco(Image img)
+        {
+            int size = img.Width;
+
+            Icon icon;
+            using (var msImg = new MemoryStream())
+            using (var msIco = new MemoryStream())
+            {
+                img.Save(msImg, ImageFormat.Png);
+                using (var bw = new BinaryWriter(msIco))
+                {
+                    bw.Write((short)0);           //0-1 reserved
+                    bw.Write((short)1);           //2-3 image type, 1 = icon, 2 = cursor
+                    bw.Write((short)1);           //4-5 number of images
+                    bw.Write((byte)size);         //6 image width
+                    bw.Write((byte)size);         //7 image height
+                    bw.Write((byte)0);            //8 number of colors
+                    bw.Write((byte)0);            //9 reserved
+                    bw.Write((short)0);           //10-11 color planes
+                    bw.Write((short)32);          //12-13 bits per pixel
+                    bw.Write((int)msImg.Length);  //14-17 size of image data
+                    bw.Write(22);                 //18-21 offset of image data
+                    bw.Write(msImg.ToArray());    // write image data
+                    bw.Flush();
+                    bw.Seek(0, SeekOrigin.Begin);
+                    icon = new Icon(msIco);
+                }
+            }
+
+            return icon;
+        }
+    }
+
