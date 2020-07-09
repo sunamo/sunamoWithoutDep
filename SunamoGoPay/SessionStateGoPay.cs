c7643953b35ab6f74c8bb6f0c4e328b6071bc7f6
Radek Jančík <sunamocz@outlook.com>
2020-07-09 20:34:59 +0200

    From VPS

--- SunamoGoPay/SessionStateGoPay.cs
+++ SunamoGoPay/SessionStateGoPay.cs
@@ -12,4 +12,13 @@ public enum SessionStateGoPay : byte
     TIMEOUTED=16,
     REFUNDED=32,
     PARTIALLY_REFUNDED = 64
+
+        //CREATED = 0,
+        //    PAYMENT_METHOD_CHOSEN = 1,
+        //    PAID = 2,
+        //    AUTHORIZED = 3,
+        //    CANCELED = 4,
+        //    TIMEOUTED = 5,
+        //    REFUNDED = 6,
+        //    PARTIALLY_REFUNDED = 7
 }
