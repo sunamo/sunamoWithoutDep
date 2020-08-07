b30232bdc1f4ca2b3fdd15bbfefdfe9ac97f7a6c
Radek Jančík <radek.jancik@sunamo.cz>
2020-08-07 17:15:18 +0200

    init

---
+++ SunamoDebugging/NameValueCollectionDebug.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class NameValueCollectionDebug
+{
+    public static void Print(NameValueCollection nvc)
+    {
+        foreach (var item in nvc.AllKeys)
+        {
+            d(item + ": " + nvc[item]);
+        }
+    }
+
+    private static void d(string v)
+    {
+        Debug.WriteLine(v);
+    }
+}
