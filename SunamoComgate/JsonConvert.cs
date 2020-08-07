5b78157849172714fdda65d900784848b892444b
Radek Jančík <radek.jancik@sunamo.cz>
2020-08-07 16:59:27 +0200

    Mass commit 7-8-2020

---
+++ SunamoComgate/JsonConvert.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class JsonConvert
+{
+    JavascriptSerialization js = new JavascriptSerialization(SerializationLibrary.Utf8Json);
+
+    internal static T DeserializeObject<T>(string v)
+    {
+        return JsonConvert.DeserializeObject<T>(v);
+    }
+}
