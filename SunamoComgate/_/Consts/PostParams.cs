c441c9d774adddb7665f8410c09c6fd3784e7699
Radek Jančík <radek.jancik@sunamo.cz>
2020-08-26 19:28:37 +0200

    Mass commit 26-8-2020

---
+++ SunamoComgate/_/Consts/PostParams.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class PostParams
+{
+    public const string code = "code";
+    public const string message = "message";
+}
---
+++ SunamoDebugging/shared/Http/HttpRequestHelperDebug.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+public class HttpRequestHelperDebug
+{
+    public static void PostParams(HttpRequest h)
+    {
+        string[] keys = h.Form.AllKeys;
+        for (int i = 0; i < keys.Length; i++)
+        {
+            var v = h.Form[keys[i]];
+
+            Debug.WriteLine(keys[i] + ": " + v);
+        }
+    }
+}
