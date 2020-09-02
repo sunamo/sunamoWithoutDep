e1f8ca73495a47477da86ff4e9a3c0aba4e6bce6
Radek Jančík <radek.jancik@sunamo.cz>
2020-09-02 23:00:01 +0200

    Mass commit 2-9-2020

---
+++ SunamoRss/RssHelper.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Rss;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SunamoRss
+{
+    public class RssHelper
+    {
+        public static List<Tuple<string, string, DateTimeOffset>> Latest5PostsFromRss(string filePath)
+        {
+            List<Tuple<string, string, DateTimeOffset>> result = new List<Tuple<string, string, DateTimeOffset>>();
+
+            using (var xmlReader = XmlReader.Create(filePath, new XmlReaderSettings()))
+            {
+                var feedReader = new RssFeedReader(xmlReader);
+
+                while (feedReader.Read().Result)
+                {
+                    switch (feedReader.ElementType)
+                    {
+                        // Read Item
+                        case SyndicationElementType.Item:
+                            ISyndicationItem item = feedReader.ReadItem().Result; //AsyncHelper.ci.GetResult(feedReader.ReadItem());
+                            result.Add(new Tuple<string, string, DateTimeOffset>(item.Title, item.Links.First().Uri.ToString(), item.Published));
+                            break;
+
+                            #region MyRegion
+                            //// Read category
+                            //case SyndicationElementType.Category:
+                            //    ISyndicationCategory category = await feedReader.ReadCategory();
+                            //    break;
+
+                            //// Read Image
+                            //case SyndicationElementType.Image:
+                            //    ISyndicationImage image = await feedReader.ReadImage();
+                            //    break;
+                            //// Read link
+                            //case SyndicationElementType.Link:
+                            //    ISyndicationLink link = await feedReader.ReadLink();
+                            //    break;
+
+                            //// Read Person
+                            //case SyndicationElementType.Person:
+                            //    ISyndicationPerson person = await feedReader.ReadPerson();
+                            //    break;
+
+                            //// Read content
+                            //default:
+                            //    ISyndicationContent content = await feedReader.ReadContent();
+                            //    break; 
+                            #endregion
+                    }
+
+                    if (result.Count == 5)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        async static Task<List<Tuple<string, string, DateTimeOffset>>> Latest5PostsFromRssAsync(string filePath)
+        {
+            List<Tuple<string, string, DateTimeOffset>> result = new List<Tuple<string, string, DateTimeOffset>>();
+
+            using (var xmlReader = XmlReader.Create(filePath, new XmlReaderSettings() { Async = true }))
+            {
+                var feedReader = new RssFeedReader(xmlReader);
+
+                while (await feedReader.Read())
+                {
+                    switch (feedReader.ElementType)
+                    {
+                        // Read category
+                        case SyndicationElementType.Category:
+                            ISyndicationCategory category = await feedReader.ReadCategory();
+                            break;
+
+                        // Read Image
+                        case SyndicationElementType.Image:
+                            ISyndicationImage image = await feedReader.ReadImage();
+                            break;
+
+                        // Read Item
+                        case SyndicationElementType.Item:
+                            ISyndicationItem item = await feedReader.ReadItem();
+
+                            result.Add(new Tuple<string, string, DateTimeOffset>(item.Title, item.Links.First().Uri.ToString(), item.Published));
+
+                            break;
+
+                        // Read link
+                        case SyndicationElementType.Link:
+                            ISyndicationLink link = await feedReader.ReadLink();
+                            break;
+
+                        // Read Person
+                        case SyndicationElementType.Person:
+                            ISyndicationPerson person = await feedReader.ReadPerson();
+                            break;
+
+                        // Read content
+                        default:
+                            ISyndicationContent content = await feedReader.ReadContent();
+                            break;
+                    }
+
+                    if (result.Count == 5)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
